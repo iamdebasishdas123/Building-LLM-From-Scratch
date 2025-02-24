96929109e9f41f1ce3cb81337dc23292b953c95b
Debasish Das <iamdebasishdas123@gmail.com>
2025-02-24 10:59:48 +0530

    add Custom dataset and Dataloader object using Pytorch

--- data_prepossing.py
+++ data_prepossing.py
@@ -1,4 +1,6 @@
 import tiktoken #ignore
+import torch
+from torch.utils.data import Dataset, DataLoader
 
 
 class Preprocessing():
@@ -10,13 +12,47 @@ class Preprocessing():
         encode=self.tokenizer.encode(self.data)
         # print(encode[:10])
         return encode
-    def decode_token_id(self,encoded_data):
-        decode=self.tokenizer.decode(encoded_data)
+    def decode_token_id(self,token_id):
+        decode=self.tokenizer.decode(token_id)
         # print(decode)
         return decode
-    def vocab(self):
-        pass
-    def create_token_ids(self):
-        pass
+
+class GPTDatasetV1(Dataset):
+    def __init__(self,token_ids, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        # # Tokenize the entire text
+        # token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
+
+        # Use a sliding window to chunk the book into overlapping sequences of max_length
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
     
-    
+def create_dataloader_v1(token_ids, batch_size=1, max_length=4,
+                         stride=128, shuffle=True, drop_last=True,
+                         num_workers=0):
+
+
+        # Create dataset
+        dataset = GPTDatasetV1(token_ids, max_length, stride)
+
+        # Create dataloader
+        dataloader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            drop_last=drop_last,
+            num_workers=num_workers
+        )
+
+        return dataloader
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from data_read import data_ingestion
-from data_prepossing import Preprocessing
+from data_prepossing import Preprocessing, create_dataloader_v1
 import importlib
 
 def main():
     data = data_ingestion()
     processed_data = Preprocessing(data)
-    processed_data.create_token_id()
-    processed_data.decode_token_id()
+    encode_data=processed_data.create_token_id()
+    # processed_data.decode_token_id()
+    dataloader=create_dataloader_v1(encode_data)
+    data_iter = iter(dataloader)
+    first_batch = next(data_iter)
+    print("Batch",first_batch)
     
 
 
